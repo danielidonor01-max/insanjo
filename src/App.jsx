854eef0fa59a13fecb8f29894bb2cdecb0924a93
Josh <ngbodinjoshua@gmail.com>
2026-07-30 15:06:21 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ResetPassword from './pages/ResetPassword'
+import CustomerResetPassword from './pages/CustomerResetPassword'
+import VendorResetPassword from './pages/VendorResetPassword'
 import PublicStore from './pages/PublicStore'
 import ViewProduct from './pages/ViewProduct'
 import Analytics from './components/Analytics'
@@ -13,6 +15,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/customers/reset-password" element={<CustomerResetPassword />} />
+      <Route path="/vendors/reset-password" element={<VendorResetPassword />} />
       <Route path="/store/:storeId" element={<PublicStore />} />
       <Route path="/product/:productId" element={<ViewProduct />} />
       <Route path="*" element={<Home />} />
